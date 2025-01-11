535c5ce41cf898530161e4b968db009902ba52b2
Gabriel R. <gabrielrengifo211@gmail.com>
2025-01-10 21:42:45 -0500

    refactor: replace App.css with App.scss, add Bootstrap styles, and integrate calculator component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import './App.scss';
+import './calculator.js'
 
 function App() {
   return (
     <div className="App">
-      
+      <header className="App-header">
+        <h1 className='display-6 text-center p-2'>Calculator</h1>
+      </header>
     </div>
   );
 }
