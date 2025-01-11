73c30ee2d0f0eab11192d2d3bb1bd441ead3c2ec
Gabriel R. <gabrielrengifo211@gmail.com>
2025-01-11 18:40:40 -0500

    feat: add state management and event handling to Calculator component

--- src/calculator.js
+++ src/calculator.js
@@ -1,6 +1,12 @@
 import './calculator.scss';
+import { useState, useEffect } from 'react';
 
 function Calculator() {
+
+    const [numbers, setNumbers] = useState([]); // proces as strings 
+    const [operators, setOperators] = useState([]); // process as strings
+
+
     return (
         <div className="calculator">
             <Display />
@@ -14,10 +20,33 @@ function KeyPad(){
     const buttonIds = ["decimal", "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
     const operationButtons = ['+', '-', '*', '/','clear', '='];
     const operationBtnIds = ["add", "subtract", "multiply", "divide", "clear", "equals"];
+
+    function handleClick(e) {
+        const origin = e.target;
+        const key = origin.getAttribute('data-key');
+        const id = origin.id;
+        const className = origin.className;
+        console.log({key, id, className});
+        
+
+    }
+
     return (
         <div className="calculator-keypad">
-            {numberButtons.map((btn, index) => {return <button className='mathBtn' key={btn} id={buttonIds[index]}>{btn}</button>})}
-            {operationButtons.map((btn, index) => {return <button className='operatorBtn' key={btn} id={operationBtnIds[index]}>{btn}</button>})}
+            {numberButtons.map((btn, index) => {return <button 
+            data-key={btn} 
+            onClick={handleClick} 
+            className='mathBtn' 
+            key={btn} 
+            id={buttonIds[index]}
+            >{btn}</button>})}
+            {operationButtons.map((btn, index) => {return <button 
+            data-key={btn} 
+            onClick={handleClick}
+            className='operatorBtn' 
+            key={btn} 
+            id={operationBtnIds[index]}
+            >{btn}</button>})}
         </div>
     );
 };
