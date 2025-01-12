19537708c0826cb8ee9dc3def2f067b271f60621
Gabriel R. <gabrielrengifo211@gmail.com>
2025-01-11 20:55:08 -0500

    feat: enhance Calculator component with joint state management and update KeyPad to utilize new functionality

--- src/calculator.js
+++ src/calculator.js
@@ -5,17 +5,27 @@ function Calculator() {
 
     const [numbers, setNumbers] = useState([]); // proces as strings 
     const [operators, setOperators] = useState([]); // process as strings
+    const [joint, setJoint] = useState([]); // process as string
+
+    const test = (val) =>{
+        setJoint((prev) => [...prev, val]);
+        
+    };
+
+    useEffect(() => {
+        console.log(joint);
+    }, [joint]);
 
 
     return (
         <div className="calculator">
             <Display />
-            <KeyPad />
+            <KeyPad functionPack={{test, setNumbers, setOperators}} />
         </div>
     );
 };
 
-function KeyPad(){
+function KeyPad({functionPack}){
     const numberButtons = ['.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
     const buttonIds = ["decimal", "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
     const operationButtons = ['+', '-', '*', '/','clear', '='];
@@ -23,10 +33,14 @@ function KeyPad(){
 
     function handleClick(e) {
         const origin = e.target;
-        const key = origin.getAttribute('data-key');
-        const id = origin.id;
-        const className = origin.className;
-        console.log({key, id, className});
+        const key = origin.getAttribute('data-key');//value of the button
+        const id = origin.id;//'name' of the button: 'seven' or 'add'
+        const className = origin.className; // 'mathBtn' or 'operatorBtn' to be identified
+        if(className === 'mathBtn'){
+            functionPack.test(key);
+        }
+
+        console.log({key, id, className, functionPack});
         
 
     }
@@ -52,6 +66,13 @@ function KeyPad(){
 };
 
 function Display(){
+
+    const [input, setInput] = useState('');
+
+    function simpleRegistry(entry){
+        setInput(input + entry)
+    }
+
     return (
         <div className="display">0</div>
     );
